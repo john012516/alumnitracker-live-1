6133c98cd822e306cccdd9da5379344c066d8d21
Dame Dela Peña <damedelapena@gmail.com>
2022-04-13 05:54:49 +0800

    Adding database connection 2

--- AlumniTracker/settings.py
+++ AlumniTracker/settings.py
@@ -105,7 +105,8 @@ WSGI_APPLICATION = 'AlumniTracker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 import dj_database_url
@@ -149,7 +150,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join('staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
